5a357261209b1dc3108eaa4def192de6fb405704
Halú de Cássia <halu.amancio@imusics.com>
2020-10-09 20:33:40 -0300

    WIP: Add game result validation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import { StyledApp, Global } from './styles/global'; 
 import Buttons from './components/Buttons'; 
+import Emoji from './components/Emoji'; 
 
 const TYPE_OPTIONS = {
-  rock: 'Pedra',
-  paper: 'Papel',
-  scissors: 'Tesoura',
-  lizard: 'Lagarto',
-  spock: 'Spock'
+  rock: ['Pedra', (<Emoji ariaLabel="pedra" emoji="✊"/>)],
+  paper: ['Papel', (<Emoji ariaLabel="papel" emoji="✋"/>)],
+  scissors: ['Tesoura', (<Emoji ariaLabel="tesoura" emoji="✌️"/>)],
+  lizard: ['Lagarto', (<Emoji ariaLabel="lagarto" emoji="🦎"/>)],
+  spock: ['Spock', (<Emoji ariaLabel="spock" emoji="🖖"/>)],
+  default: ['Escolha uma opção para começar', '']
 }
 
 function getComputerChoice() {
@@ -16,19 +18,40 @@ function getComputerChoice() {
   return computerChoice[Math.floor(Math.random()*computerChoice.length)];
 }
 
+function getResult(playerChoice, computerChoice, setResult) {
+  const rock_wins = ['lizard', 'scissors'];
+  const papers_wins = ['rock', 'spock'];
+  const scissors_wins = ['paper', 'lizard'];
+  const lizard_wins = ['spock', 'paper'];
+  const spock_wins = ['tesoura', 'pedra'];
+
+  if(playerChoice === computerChoice) {
+
+  }
+}
+
 function App() {
-  const [playerChoice, setPlayerChoice] = React.useState('');
+  const [playerChoice, setPlayerChoice] = React.useState(null);
+  const [computerChoice, setComputerChoice] = React.useState('');
+  const [result, setResult] = React.useState('');
 
   const playGame = (playerChoice) => {
     setPlayerChoice(playerChoice);
-    alert(TYPE_OPTIONS[getComputerChoice()]);
+    setComputerChoice(getComputerChoice());
   }
 
   return (
     <StyledApp>
       <Global />
-      <Buttons setOption={ playGame } />
-      {TYPE_OPTIONS[playerChoice]}
+      <Buttons playGame={ playGame } />
+      {TYPE_OPTIONS[playerChoice || 'default'][1]}
+      {TYPE_OPTIONS[playerChoice || 'default'][0]}
+      {computerChoice && (
+        <React.Fragment>
+          {TYPE_OPTIONS[computerChoice][1]}
+          {TYPE_OPTIONS[computerChoice][0]}
+        </React.Fragment>
+      )}
     </StyledApp>
   );
 }
