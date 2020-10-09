fe5ee321f00ac3c582949b0fa39afce93dc9e46a
Halú de Cássia <halu.amancio@imusics.com>
2020-10-09 20:32:54 -0300

    Alter props function name of Buttons component

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Button, Grid } from './styles'; 
 
-export default function Buttons({ setOption }) {
+export default function Buttons({ playGame }) {
   return (
     <Grid>
-      <Button onClick={() => setOption('rock')}><span role="img" aria-label="pedra">✊</span></Button>
-      <Button onClick={() => setOption('paper')}><span role="img" aria-label="papel">✋</span></Button>
-      <Button onClick={() => setOption('scissors')}><span role="img" aria-label="tesoura">✌️</span></Button>
-      <Button onClick={() => setOption('lizard')}><span role="img" aria-label="lagarto">🦎</span></Button>
-      <Button onClick={() => setOption('spock')}><span role="img" aria-label="spock">🖖</span></Button>
+      <Button onClick={() => playGame('rock')}><span role="img" aria-label="pedra">✊</span></Button>
+      <Button onClick={() => playGame('paper')}><span role="img" aria-label="papel">✋</span></Button>
+      <Button onClick={() => playGame('scissors')}><span role="img" aria-label="tesoura">✌️</span></Button>
+      <Button onClick={() => playGame('lizard')}><span role="img" aria-label="lagarto">🦎</span></Button>
+      <Button onClick={() => playGame('spock')}><span role="img" aria-label="spock">🖖</span></Button>
     </Grid>
   );
 }
+
+Buttons.propTypes = {
+  playGame: PropTypes.func.isRequired
+};
