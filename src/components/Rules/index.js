d689de94c258f05cf955c037f0b9797a220921d6
Halu de Cassia <haluanedecassia@gmail.com>
2020-10-11 16:14:13 -0300

    Add Rules component

---
+++ src/components/Rules/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Modal from '../Modal'; 
+
+import { Button, StyledRules } from './styles.js';
+
+export default function App({ playerChoice, computerChoice, result}) {
+  const [modal, toggleModal] = React.useState(false);
+
+  return (
+    <StyledRules>
+      <p>
+        Pedra-papel-tesoura-lagarto-Spock é uma expansão do clássico método de seleção em jogo de pedra-papel-tesoura. O jogo foi inventado por Sam Kass e Karen Bryla, como "Rock Paper Scissors Lizard Spock". Para entender as regras, 
+        <Button onClick={() => toggleModal(!modal)} aria-label="ver regras">clique aqui</Button>
+      </p>
+
+      {modal && <Modal isOpen={modal} onClose={toggleModal} /> }
+    </StyledRules>
+  );
+}
---
+++ src/components/Rules/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledRules = styled.div`
+  margin: 2rem;
+  text-align: center;
+`;
+
+export const Button = styled.button`
+  border: none;
+  color: inherit;
+  cursor: pointer;
+  font-size: inherit;
+  font-family: inherit;
+  background: transparent;
+  text-decoration: underline;
+`;
