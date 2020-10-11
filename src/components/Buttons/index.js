434c9771ae18ef6752c50ae8e8fc75c3dacf88cf
Halu de Cassia <haluanedecassia@gmail.com>
2020-10-11 19:35:21 -0300

    WIP: Add style in Buttons component

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -2,17 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Grid from '../Grid';
+import Emoji from '../Emoji';
 
 import { Button } from './styles'; 
+
+const options = [
+  {
+    name: 'rock',
+    label: 'pedra',
+    emoji: 'hand-rock',
+    color: '#CE5788'
+  }, {
+    name: 'paper',
+    label: 'papel',
+    emoji: 'hand-paper',
+    color: '#CE5788'
+  }, {
+    name: 'scissors',
+    label: 'tesoura',
+    emoji: 'hand-scissors',
+    color: '#CE5788'
+  }, {
+    name: 'lizard',
+    label: 'lagarto',
+    emoji: 'hand-lizard',
+    color: '#CE5788'
+  }, {
+    name: 'spock',
+    label: 'spock',
+    emoji: 'hand-spock',
+    color: '#CE5788'
+  }
+];
 
 export default function Buttons({ playGame }) {
   return (
     <Grid>
-      <Button onClick={() => playGame('rock')}><span role="img" aria-label="pedra">✊</span></Button>
-      <Button onClick={() => playGame('paper')}><span role="img" aria-label="papel">✋</span></Button>
-      <Button onClick={() => playGame('scissors')}><span role="img" aria-label="tesoura">✌️</span></Button>
-      <Button onClick={() => playGame('lizard')}><span role="img" aria-label="lagarto">🦎</span></Button>
-      <Button onClick={() => playGame('spock')}><span role="img" aria-label="spock">🖖</span></Button>
+      {options.map((option, index) => (
+        <Button onClick={() => playGame(option.name)}>
+          <Emoji gradient size={4} color={option.color} ariaLabel={option.label} emoji={option.emoji}/>
+        </Button>
+      ))}
     </Grid>
   );
 }
