9e06cdbf54d9ce43b925b6a53650513def411c5a
Halu de Cassia <haluanedecassia@gmail.com>
2020-10-12 13:39:42 -0300

    Updates

---
+++ precache-manifest.f79cd6362ac80989794a152f76c4fff1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d7a3e7a171ea2e7e3df3d757935fe9",
+    "url": "/lizard-spock/index.html"
+  },
+  {
+    "revision": "3809eaf19468e760ca04",
+    "url": "/lizard-spock/static/css/2.b3bc2287.chunk.css"
+  },
+  {
+    "revision": "3809eaf19468e760ca04",
+    "url": "/lizard-spock/static/js/2.7fec5c17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lizard-spock/static/js/2.7fec5c17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9ae1228ec90ea2ebad",
+    "url": "/lizard-spock/static/js/main.c50850be.chunk.js"
+  },
+  {
+    "revision": "46e38739475c9f3119be",
+    "url": "/lizard-spock/static/js/runtime-main.c9e50a9b.js"
+  },
+  {
+    "revision": "f40889caa25956ef4d27c2aadc4fbe13",
+    "url": "/lizard-spock/static/media/Comfortaa-VariableFont_wght.f40889ca.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/lizard-spock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/lizard-spock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/lizard-spock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/lizard-spock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/lizard-spock/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
